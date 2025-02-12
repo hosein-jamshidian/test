9152c29856177f69df36b864305c7f68c8a144ab
hosein jamshidian <93486278+hosein-jamshidian@users.noreply.github.com>
2025-02-12 21:35:48 +0330

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -1,9 +1,10 @@
 import pandas as pd 
 import numpy as np 
+import os
 
-key= "my_key"
+api_key = os.getenv('TEST_KEY')
 
-print(key)
+print(api_key)
 data= pd.DataFrame({"A":[1,2,3], "B": [10,11,12], "C": [20,21,22]})
 
 print("Done!")
